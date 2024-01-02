e005a89369ae9b33cb9158e8bf65c8b1c4d1cfd3
joearshad <61275980+joearshad@users.noreply.github.com>
2024-01-02 10:36:04 +0200

    Add files via upload

---
+++ JunitTester/src/main/java/org/example/Service.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Service {
+
+    public String getName(int number) {
+        if(number < 0) {
+            return "a";
+        }
+        else {
+            return "b";
+        }
+    }
+}
